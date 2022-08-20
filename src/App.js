93ae51e9951b15cc551d63c971652cff090bc037
Roney Fernandes <102328384+ReD-VII@users.noreply.github.com>
2022-08-20 16:46:36 -0300

    UP

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './HC3.png';
 import './App.css';
 
 function App() {
@@ -7,16 +7,16 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p style={{marginTop: "150px"}}>
+          Estúdio digital HC3 House.
         </p>
-        <a
+        <a style={{marginTop: "150px"}}
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Saiba mais
         </a>
       </header>
     </div>
